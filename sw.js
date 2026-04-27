5f8ec53027208fae4749d0a925bb45289de4eab9
joaodasilva582-del <joaodasilva582@gmail.com>
2026-04-27 11:29:41 -0300

    Refactor service worker to improve caching logic

--- sw.js
+++ sw.js
@@ -1,17 +1,21 @@
 const CACHE_NAME = 'spiltag-v1';
 const ASSETS = [
-  'index.html',
-  'manifest.json'
+  '/inventario-resina/index.html',
+  '/inventario-resina/manifest.json'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
