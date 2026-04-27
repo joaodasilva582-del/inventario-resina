8e1d2580bc4a5f34267b20f79cd000ce56728633
joaodasilva582-del <joaodasilva582@gmail.com>
2026-04-27 14:53:58 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,22 @@
-const CACHE_NAME = "spiltag-v1";
+const CACHE_NAME = 'spiltag-v4';
+const ASSETS = [
+  '/inventario-resina/',
+  '/inventario-resina/index.html',
+  '/inventario-resina/manifest.json'
+];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
     })
   );
 });
